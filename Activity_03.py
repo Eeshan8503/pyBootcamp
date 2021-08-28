b737c62f8031dd6785afd4ec605ce69da5e43750
Eeshan Mattey <71863350+Eeshan8503@users.noreply.github.com>
2021-08-28 13:21:13 +0530

    Update Activity_03.py

--- Activity_03.py
+++ Activity_03.py
@@ -2,3 +2,8 @@ string1=(input("Enter String 1"))
 string2=(input("Enter String 2"))
 print(string1+string2)
 print((string1+string2)*5)
+//////////////////////
+string1=(input("Enter String 1"))
+string2=(input("Enter String 2"))
+print(string1+string2)
+print((string1+" "+string2+"\n")*5)
